a0cd1476192e020b980a9ed4abae2dae2fd25fe2
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-12 06:53:01 +0800

    Fix

--- src/features/productionRecord/api/index.js
+++
@@ -1,7 +0,0 @@
-import addProductionRecord from './addProductionRecord.js'
-import getProductionRecords from './getProductionRecords.js'
-
-export {
-  addProductionRecord,
-  getProductionRecords
-}

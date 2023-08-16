b5fc6bd815c7c4efd4e8fe81addee9b69c75c306
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-16 08:24:01 +0800

    Fix

--- src/main.js
+++ src/main.js
@@ -1,36 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
-
-// import {
-//   CfActionCard,
-//   CfAppLayout,
-//   CfAppView,
-//   CfAppViewHeader,
-//   CfBanner,
-//   CfBreadcrumbs,
-//   CfFilledButton,
-//   CfOutlinedButton,
-//   CfTextButton,
-//   CfChoice,
-//   CfChoiceList,
-//   CfDataTable,
-//   CfDialog,
-//   CfDropdown,
-//   CfDropdownItem,
-//   CfHeader,
-//   CfInput,
-//   CfSelect,
-//   CfSummaryList,
-//   CfSwitch,
-//   CfTag
-// } from 'vue-cf-ui'
 import 'vue-cf-ui/dist/style.css'
 
 const app = createApp(App)
 
 app.use(router)
-
-// app.component('CfActionCard', CfActionCard)
-
 app.mount('#app')

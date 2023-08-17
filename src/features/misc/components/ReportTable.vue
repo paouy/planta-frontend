5498115e990efa4679f0954605346334c8436269
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-17 08:24:47 +0800

    Fix

--- src/features/misc/components/ReportTable.vue
+++ src/features/misc/components/ReportTable.vue
@@ -5,20 +5,11 @@ const props = defineProps({ productionRecords: Array })
 
 const columns = [
   {
-    label: 'Date',
-    key: 'timestamp'
-  }, {
     label: 'ID',
     key: 'productionOrderPublicId'
   }, {
     label: 'Operation',
     key: 'operation.name'
-  }, {
-    label: 'Type',
-    key: 'type'
-  }, {
-    label: 'Qty',
-    key: 'qty'
   }, {
     label: 'Workstation',
     key: 'workstation.name'
@@ -26,8 +17,15 @@ const columns = [
     label: 'Equipment',
     key: 'equipment.name'
   }, {
+    label: 'Qty',
+    key: 'qty',
+    width: '10%'
+  }, {
     label: 'Worker',
     key: 'worker.name'
+  }, {
+    label: 'Date',
+    key: 'timestamp'
   }
 ]
 </script>
@@ -39,14 +37,13 @@ const columns = [
     searchable
   >
     <template v-slot:row="{ data }">
-      <td>{{ new Date(data.timestamp).toLocaleDateString('en-CA') }}</td>
       <td>{{ data.productionOrderPublicId }}</td>
       <td>{{ data.operation.name }}</td>
-      <td>{{ data.type }}</td>
-      <td>{{ data.qty }}</td>
       <td>{{ data.workstation?.name }}</td>
       <td>{{ data.equipment?.name }}</td>
+      <td>{{ (data.type === 'REJECT' ? '(' : '') + data.qty + (data.type === 'REJECT' ? ')' : '') }}</td>
       <td>{{ data.worker?.name }}</td>
+      <td>{{ new Date(data.timestamp).toLocaleDateString('en-CA') }}</td>
     </template>
   </CfDataTable>
 </template>

68f29b45808df0b2972e70105ad585b171b579e4
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-17 10:15:48 +0800

    Fix

--- src/features/productionRecord/components/ProductionRecordsList.vue
+++ src/features/productionRecord/components/ProductionRecordsList.vue
@@ -8,14 +8,18 @@ const columns = [
     label: 'Date',
     key: 'timestamp'
   }, {
-    label: 'Operation',
-    key: 'operation.name'
+    label: 'Workstation',
+    key: 'workstation.name'
   }, {
-    label: 'Type',
-    key: 'type'
+    label: 'Equipment',
+    key: 'equipment.name'
   }, {
     label: 'Quantity',
     key: 'qty'
+  }, {
+    label: 'Worker',
+    key: 'worker.name',
+    width: '30%'
   }
 ]
 </script>
@@ -27,9 +31,10 @@ const columns = [
   >
     <template v-slot:row="{ data }">
       <td>{{ new Date(data.timestamp).toLocaleDateString('en-CA') }}</td>
-      <td>{{ data.operation.name }}</td>
-      <td>{{ data.type }}</td>
-      <td>{{ data.qty }}</td>
+      <td>{{ data.workstation?.name }}</td>
+      <td>{{ data.equipment?.name }}</td>
+      <td>{{ (data.type === 'REJECT' ? '(' : '') + data.qty + (data.type === 'REJECT' ? ')' : '') }}</td>
+      <td>{{ data.worker?.name }}</td>
     </template>
   </CfDataTable>
 </template>

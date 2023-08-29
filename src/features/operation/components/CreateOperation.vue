0c647df26cb974692e81b4f5e340aa21b747c5d4
Paolo Uy <paolo.uy@obf.ateneo.edu>
2023-08-29 08:03:23 +0800

    Fix operation batch size parameter

--- src/features/operation/components/CreateOperation.vue
+++ src/features/operation/components/CreateOperation.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { ref } from 'vue'
-import { CfDialog, CfInput, CfSwitch, CfFilledButton } from 'vue-cf-ui'
+import { ref, computed } from 'vue'
+import { CfDialog, CfInput, CfSwitch, CfSelect, CfFilledButton } from 'vue-cf-ui'
 import api from '../../../api/index.js'
 
 const emit = defineEmits(['success', 'cancel'])
 const props = defineProps({ lastSeq: Number })
 
 const isLoading = ref(false)
+const metafieldOptions = ref([])
 const ctx = ref({
   name: '',
   seq: (props.lastSeq || 0) + 1,
@@ -14,6 +15,13 @@ const ctx = ref({
   allowsRework: true,
   isBatch: false,
   batchSizeParameter: null
+})
+
+const parameterOptions = computed(() => {
+  return [
+    { label: 'Total units', value: 'TOTAL_UNITS' },
+    ...metafieldOptions.value
+  ]
 })
 
 const invoke = async () => {
@@ -30,6 +38,13 @@ const invoke = async () => {
     isLoading.value = false
   }
 }
+
+api.metafield.getAll({ resource: 'PRODUCT' }).then(data => {
+  metafieldOptions.value = data.map(({ id, name }) => ({
+    label: name,
+    value: id
+  }))
+})
 </script>
 
 <template>
@@ -57,9 +72,10 @@ const invoke = async () => {
           v-model="ctx.isBatch"
           label="Run by batch"
         />
-        <CfInput
+        <CfSelect
           v-model="ctx.batchSizeParameter"
           label="Batch size parameter"
+          :options="parameterOptions"
           required
           v-if="ctx.isBatch"
         />
